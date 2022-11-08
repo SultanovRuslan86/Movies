c89df9eeba843a636b8abbbe60a4b205b5e4655f
SultanovRuslan86 <sulrus1986@gmail.com>
2022-11-08 11:00:33 +0600

    Initial commit

---
+++ django_movie/contact/forms.py
@@ -0,0 +1,19 @@
+from django import forms
+from .models import Contact
+from snowpenguin.django.recaptcha3.fields import ReCaptchaField
+
+
+
+class ContactForm(forms.ModelForm):
+    """форма подписки на емэйл"""
+    captcha = ReCaptchaField()
+
+    class Meta:
+        model = Contact
+        fields = ("email", "captcha")
+        widgets = {
+            "email": forms.TextInput(attrs={"class": "editContent", "placeholder": "Your Email..."})
+        }
+        labels = {
+            "email": ''
+        }
